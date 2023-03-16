e3e0fbd6df65c7a34ba6ef299fdc4d0f3db29595
Aleksey Zhevlakov <aszhev@yandex.ru>
2023-03-16 08:44:43 +0300

    refactor: в соответствии с замечаниями ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,44 +7,68 @@ const popupProfileEdit = document.querySelector('.popup_type_profile-edit');
 const formProfileEdit = popupProfileEdit.querySelector('.form');
 const profileNameInput = formProfileEdit.querySelector('.form__item_el_name');
 const profileJobInput = formProfileEdit.querySelector('.form__item_el_job');
-const popupProfileEditClose = popupProfileEdit.querySelector('.popup__close');
 
 // Card
 const cardsGallery = document.querySelector('.photo-gallery__items');
 const cardAdd = document.querySelector('.profile__photo-card-add');
+const cardTemplate = document.querySelector('#photo-card-template').content;
 
 const popupCardAdd = document.querySelector('.popup_type_photo-card-add');
 const formCardAdd = popupCardAdd.querySelector('.form');
-const popupCardAddClose = popupCardAdd.querySelector('.popup__close');
+const cardNameInput = formCardAdd.querySelector('.form__item_el_card-name');
+const cardLinkInput = formCardAdd.querySelector('.form__item_el_card-link');
 
 // Img
 const popupImg = document.querySelector('.popup_type_img-open');
-const popupImgClose = popupImg.querySelector('.popup__close');
+const imgPopupImg = popupImg.querySelector('.photo-viewer__image');
+const captionPopupImg = popupImg.querySelector('.photo-viewer__description');
 
+
+const popupsClose = document.querySelectorAll('.popup__close');
+
+popupsClose.forEach((item) => {
+  const popup = item.closest('.popup');
+  item.addEventListener('click', () => {
+    closePopup(popup);
+  });
+});
+
+
+// Функция открытия попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+// Функция закрытия попапов
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
 
 // Инициализация фотокарточки
-const initCard = function (cardName, link) {
-  const cardTemplate = document.querySelector('#photo-card-template').content;
+const initCard = function (name, link) {
   const card = cardTemplate.querySelector('.photo-card').cloneNode(true);
-
-  card.querySelector('.photo-card__image').src = link;
-  card.querySelector('.photo-card__image').alt = cardName;
-  card.querySelector('.photo-card__name').textContent = cardName;
-
   const cardRemove = card.querySelector('.photo-card__delete');
   const cardLike = card.querySelector('.photo-card__like');
-  const imgOpen = card.querySelector('.photo-card__image');
+  const cardImg = card.querySelector('.photo-card__image');
+  const cardName = card.querySelector('.photo-card__name');
 
+  cardImg.src = link;
+  cardImg.alt = name;
+  cardName.textContent = name;
+
+  // Обработчик нажатия на кнопку удаления карточки
   cardRemove.addEventListener('click', removeCard);
+
+  // Обработчик нажатия на лайк в карточке
   cardLike.addEventListener('click', likeCard);
 
-  // Слушатель нажатия по изображению
-  imgOpen.addEventListener('click', () => {
-    popupImg.querySelector('.photo-viewer__image').src = link;
-    popupImg.querySelector('.photo-viewer__image').alt = cardName;
-    popupImg.querySelector('.photo-viewer__description').textContent = cardName;
+  // Обработчик нажатия по изображению
+  cardImg.addEventListener('click', () => {
+    imgPopupImg.src = link;
+    imgPopupImg.alt = name;
+    captionPopupImg.textContent = name;
 
-    popupImg.classList.add('popup_opened');
+    openPopup(popupImg);
   });
 
   return card;
@@ -83,37 +107,10 @@ function addCardsOnLoad() {
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
+
   initialCards.forEach((item) => {
     addCard(initCard(item.cardName, item.link), cardsGallery);
   });
-}
-
-// Открытие попапа редактирования профиля
-function openPopupProfileEdit() {
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
-
-  popupProfileEdit.classList.add('popup_opened');
-}
-
-// Закрытие попапа редактирования профиля
-function closePopupProfileEdit() {
-  popupProfileEdit.classList.remove('popup_opened');
-}
-
-// Открытие попапа добавления новой фотокарточки
-function openPopupCardAdd() {
-  popupCardAdd.classList.add('popup_opened');
-}
-
-// Закрытие попапа добавления новой фотокарточки
-function closePopupCardAdd() {
-  popupCardAdd.classList.remove('popup_opened');
-}
-
-// Закрытие попапа просмотра изображения
-function closePopupImg() {
-  popupImg.classList.remove('popup_opened');
 }
 
 // Удаление фотокарточки
@@ -126,39 +123,40 @@ function likeCard(evt) {
   evt.target.classList.toggle('photo-card__like_active');
 }
 
+
+// Слушатели событий
+
+// Обработчик нажатия на кнопку редактирования профиля
+profileEdit.addEventListener('click', () => {
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+
+  openPopup(popupProfileEdit);
+});
+
 // Обработчик отправки формы редактирования профиля
-function handleFormProfileEditSubmit(evt) {
+formProfileEdit.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   profileName.textContent = profileNameInput.value;
   profileJob.textContent = profileJobInput.value;
 
-  closePopupProfileEdit();
-}
+  closePopup(popupProfileEdit);
+});
+
+// Обработчик нажатия на кнопку добавления новой карточки
+cardAdd.addEventListener('click', () => {
+  openPopup(popupCardAdd);
+});
 
 // Обработчик отправки формы создания новой фотокарточки
-function handleFormCardAddSubmit(evt) {
+formCardAdd.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const cardNameInput = formCardAdd.querySelector('.form__item_el_card-name');
-  const cardLinkInput = formCardAdd.querySelector('.form__item_el_card-link');
-
   addCard(initCard(cardNameInput.value, cardLinkInput.value), cardsGallery);
+  closePopup(popupCardAdd);
+  evt.target.reset();
+});
 
-  closePopupCardAdd();
-
-  cardNameInput.value = '';
-  cardLinkInput.value = '';
-}
-
-// Слушатели событий
-profileEdit.addEventListener('click', openPopupProfileEdit);
-formProfileEdit.addEventListener('submit', handleFormProfileEditSubmit);
-popupProfileEditClose.addEventListener('click', closePopupProfileEdit);
-cardAdd.addEventListener('click', openPopupCardAdd);
-formCardAdd.addEventListener('submit', handleFormCardAddSubmit);
-popupCardAddClose.addEventListener('click', closePopupCardAdd);
-popupImgClose.addEventListener('click', closePopupImg);
 
 // Добавление набора фотокарточек при загрузке страницы
 addCardsOnLoad();
